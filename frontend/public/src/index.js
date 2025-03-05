f94fef75480a283e2a45413df28ac42e5e3540a3
anandazaugust <164444067+anandazaugust@users.noreply.github.com>
2025-03-05 09:15:55 +0530

    Update index.js

--- frontend/public/src/index.js
+++ frontend/public/src/index.js
@@ -1,9 +1,8 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
-import App from './App';
-import reportWebVitals from './reportWebVitals';
-
+import App from './App';  // Correctly import App.js
+// import reportWebVitals from './reportWebVitals'; // Removed
 ReactDOM.render(
   <React.StrictMode>
     <App />
@@ -11,6 +10,4 @@ ReactDOM.render(
   document.getElementById('root')
 );
 
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-reportWebVitals();
+// reportWebVitals(); // Removed
